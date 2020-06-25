0f6e87e0d15c6f04516a6a61748c07f8d164e711
admin <daominhhiep97@gmail.com>
2020-06-25 23:30:15 +0700

    keydown event

--- move_number.js
+++ move_number.js
@@ -1,2 +1,19 @@
-
-
+function k_key() {
+    switch (window.event.keyCode) {
+        case 37:
+            console.log('trai');
+            break;
+        case 39:
+            console.log('phai');
+            break;
+        case 38:
+            console.log('len');
+            break;
+        case 40:
+            console.log('xuong');
+            break;
+    }
+}
+document.onkeydown=function () {
+    k_key()
+};

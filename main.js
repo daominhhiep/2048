1b2bd4ec2d2d7b54f9193f1ebac0f841e2efda15
admin <daominhhiep97@gmail.com>
2020-06-28 22:56:01 +0700

    thêm màu cho các ô và thêm ghi chú

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ let main=[
     [12,13,14,15]
 ];
 
-function myFunction(){
-    let key = Math.floor(Math.random()*16);
+function myFunction(){              //Khi vào game tạo sẵn 1 số ngẫu nhiên
+    let key = Math.floor(Math.random() * 16);
     if(cell[key].innerHTML == ""){
-        cell[key].innerHTML = Math.random()<0.85? 2:4;
+        cell[key].innerHTML = Math.random() < 0.85 ? 2:4;
         designNumber();
     }
 }
@@ -42,9 +42,9 @@ function startGame(event){
 }
 
 function randomNumber(){
-    let ramdomNum = Math.floor(Math.random()*16);
-    if(cell[ramdomNum].innerHTML===""){
-        cell[ramdomNum].innerHTML=Math.random()<0.85? 2:4;
+    let ramdomNum = Math.floor(Math.random() * 16);
+    if(cell[ramdomNum].innerHTML === ""){                          // Nếu ô trống
+        cell[ramdomNum].innerHTML = Math.random() < 0.85 ? 2:4;   //xuất hiện ngẫu nhiên số 2 hoặc 4 với tỷ lệ số 2 là 85%
         console.log("working");
     }
     else{
@@ -55,19 +55,19 @@ function randomNumber(){
 function moveUp() {
     for (let j = 0; j < 4; j++) {
         for (let i = 1; i <4; i++) {
-            if (cell[main[i][j]].innerHTML) {
+            if (cell[main[i][j]].innerHTML) {               // Nếu ô cell có giá trị
                 let row = i;
                 while(row > 0){
-                    if (cell[main[row-1][j]].innerHTML == "") {
-                        cell[main[row-1][j]].innerHTML = cell[main[row][j]].innerHTML;
-                        cell[main[row][j]].innerHTML = "";
+                    if (cell[main[row-1][j]].innerHTML == "") {             //Nếu ô phía trên ô cell là ô trống
+                        cell[main[row-1][j]].innerHTML = cell[main[row][j]].innerHTML;  //Di chuyển giá trị ô cell lên ô trống
+                        cell[main[row][j]].innerHTML = "";                  // Cho giá trị ô cell vừa lên thành trống
                         row--;
                     }
-                    else if(cell[main[row-1][j]].innerHTML == cell[main[row][j]].innerHTML)
+                    else if(cell[main[row-1][j]].innerHTML == cell[main[row][j]].innerHTML)   //Nếu ô phía trên ô cell có giá trị = với ô cell
                     {
-                        cell[main[row-1][j]].innerHTML *= 2;
-                        cell[main[row][j]].innerHTML = "";
-                        score.innerHTML = parseInt(score.innerHTML) + parseInt(cell[main[row-1][j]].innerHTML);
+                        cell[main[row-1][j]].innerHTML *= 2;            // Cho giá trị ô phía trên cell nhân 2 lên
+                        cell[main[row][j]].innerHTML = "";              // Đưa giá trị ô cell về trống
+                        score.innerHTML = parseInt(score.innerHTML) + parseInt(cell[main[row-1][j]].innerHTML);     // Cộng giá trị 2 ô vào score
                         break;
                     }
                     else {

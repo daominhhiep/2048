40c7b0b31c3caa7713759adb9419cd4dfb989988
admin <daominhhiep97@gmail.com>
2020-06-29 13:52:10 +0700

    Thêm hiệu ứng css cho tiêu đề

---
+++ script/Config.js
@@ -0,0 +1,16 @@
+const KEY_LEFT = 37;
+const KEY_UP = 38;
+const KEY_RIGHT = 39;
+const KEY_DOWN = 40;
+const KEY_A = 65;
+const KEY_D = 68;
+const KEY_S = 83;
+const KEY_W = 87;
+const cell = document.getElementsByClassName("grid_cell");
+const score = document.getElementById("score");
+const main = [
+    [0,1,2,3],
+    [4,5,6,7],
+    [8,9,10,11],
+    [12,13,14,15]
+            ];

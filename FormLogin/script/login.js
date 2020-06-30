2c3885cf597e7dc99854f7ab9208b0dce1f3cc7a
admin <daominhhiep97@gmail.com>
2020-06-30 11:58:17 +0700

    add background vào form

--- FormLogin/script/login.js
+++ FormLogin/script/login.js
@@ -1,54 +1,37 @@
 const DEFAULT_USERNAME = "admin";
 const DEFAULT_PASSWORD = "123456";
+let input = document.querySelectorAll("input");
+let button = document.querySelectorAll("button");
 
 function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
-    if (username !== DEFAULT_USERNAME || password !== DEFAULT_PASSWORD){
-        alert("Đăng nhập thất bại, mời đăng nhập lại!");
+    if (username !== DEFAULT_USERNAME || password !== DEFAULT_PASSWORD) {
+        swal("Oops!", "Đăng nhập thất bại, mời đăng nhập lại!", "error");
         return false;
-    }else if (username === DEFAULT_USERNAME && password === DEFAULT_PASSWORD){
-        alert("Đăng nhập thành công!")
+    } else if (username === DEFAULT_USERNAME && password === DEFAULT_PASSWORD){
+        return true;
     }
 }
 
-
-
-
-//         if (user == DEFAULT_USERNAME) {
-//             if (pass == "") {
-//                 swal("Mật khẩu không được để trống.");
-//                 break;
-//             }
-//             if (pass == DEFAULT_PASSWORD) {
-//                 alert("Đăng nhập thành công");
-//                 break;
-//             } else {
-//                 alert("Bạn đã nhập sai mật khẩu");
-//                 break;
-//             }
-//         }
-//         else if (user == "" || pass == "") {
-//             swal ("Tên tài khoản và mật khẩu không được để trống.");
-//             break;
-//         }
-//         else if (user == "") {
-//             swal ("Tên tài khoản không được để trống.");
-//             break;
-//         }
-//         else if (pass == "") {
-//             swal("Mật khẩu không được để trống.");
-//             break;
-//         }
-//         else if (user !== DEFAULT_USERNAME) {
-//             alert("Bạn chưa đăng ký hoặc tên tài khoản sai!");
-//             break;
-//         }
-//     }
-//     return true;
-// }
-
-
-
-
-
+function check() {
+    if (input[0].value != "" && input[1].value != "")
+        button.disabled = false;
+    else {
+        button.disabled = true;
+    }
+}
+let eye = document.querySelector(".pass");
+eye.addEventListener("click",function () {
+    if(this.classList[2]== "fa-eye"){
+        this.classList.remove("fa-eye");
+        this.classList.add("fa-eye-slash");
+        input[1].type="text";
+    } else {
+        this.classList.remove("fa-eye-slash");
+        this.classList.add("fa-eye");
+        input[1].type="password";
+    }
+})
+input[0].addEventListener("keyup",check);
+input[1].addEventListener("keyup",check);

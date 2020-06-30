5cfa3e0b29f84f8d783b5fd9ce6039862b9f6f42
admin <daominhhiep97@gmail.com>
2020-06-30 10:01:30 +0700

    update

--- script/Config.js
+++ script/Config.js
@@ -15,6 +15,6 @@ const main = [
     [8,9,10,11],
     [12,13,14,15]
             ];
-let audio1 = new Audio('audio/audio.mp3');
-let audio2 = new Audio('audio/background_audio.mp3')
-let audio3 = new Audio('audio/background_audio2.mp3')
+const audio1 = new Audio('audio/audio.mp3');
+const audio2 = new Audio('audio/background_audio.mp3')
+const audio3 = new Audio('audio/background_audio2.mp3')
